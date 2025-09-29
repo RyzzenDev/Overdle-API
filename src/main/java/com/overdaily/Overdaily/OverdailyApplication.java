871e4848ccb559d736f74b15ff37000bea142b56
unknown <gabrieltupinambafamilia@gmail.com>
2025-09-29 11:55:41 -0300

    primeiro commit do codigo

---
+++ src/main/java/com/overdaily/Overdaily/OverdailyApplication.java
@@ -0,0 +1,13 @@
+package com.overdaily.Overdaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverdailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverdailyApplication.class, args);
+	}
+
+}
